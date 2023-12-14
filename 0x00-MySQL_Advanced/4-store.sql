944e4281f76d101aa16f8adee715e819c92ae72d
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-14 05:49:47 +0300

    task4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,11 @@
 -- A script that creates a trigger that decreases the quantity of an item after adding a new order
 -- Quantity in the items' table can be negative
+
 DROP TRIGGER IF EXISTS minimize_quantity;
 
 DELIMITER //
 CREATE TRIGGER minimize_quantity
-AFTER INSERT ON orders
+AFTER INSERT IN orders
 FOR EACH ROW
 BEGIN
     UPDATE items
