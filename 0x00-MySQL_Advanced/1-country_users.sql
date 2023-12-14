f7255de165b27398b314df80dfeb4e4099968cd5
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-14 03:40:17 +0300

    task1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- A script that creates a SQL table
+-- Table has the following attributes:
+-- (id, email, name, country)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
