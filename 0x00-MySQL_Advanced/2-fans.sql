82e6e56163ecb0455e753b775c19a057470afe57
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-14 04:14:20 +0300

    task2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 -- Ordered by the number of fans
 -- Requirements: Imports table, col origin  nb_fans
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, COUNT(*) AS nb_fans, DENSE_RANK() OVER (ORDER BY nb_fans DESC) AS rank
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
