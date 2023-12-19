15ed2fffe97c59bd875f3e4e08a9e7c96038c937
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-20 00:45:39 +0300

    task9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,5 +9,5 @@ def insert_school(mongo_collection, **kwargs):
     A function that insert a doc in a collection
     """
     new_doc = kwargs
-    result = mongo_collection.insert_one(new-doc)
-    return result
+    result = mongo_collection.insert_one(new_doc)
+    return result.inserted_id
