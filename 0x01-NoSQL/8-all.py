2ff4b723df920ba48e733b25d78b8ccd99e720cf
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-20 00:25:31 +0300

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,12 +2,10 @@
 """
 A module that implements list_all(mongo_collection):
 """
-from typing import List
 
 
-def list_all(mongo_collection: Any) -> List[Any]:
+def list_all(mongo_collection: Collection) -> List[Dict]:
     """
     A function that lists all documents in a collection
     """
-    all_doc = list(mongo_collection.find())
-    return all_doc
+    return [doc for doc in mongo_collection.find()]
