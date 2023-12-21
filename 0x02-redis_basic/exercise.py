c6813c30373d0cf55c6c3f7b9202808d3a0ed39b
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-21 04:38:14 +0300

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         Initializes a Redis connection and flushes the database
         """
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
