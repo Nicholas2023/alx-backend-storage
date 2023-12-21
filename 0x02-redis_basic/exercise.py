5cc877c2aa58e31d35a1a0e863a1198d555a2feb
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-21 04:33:47 +0300

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the provided data in Redis with a generated random key
